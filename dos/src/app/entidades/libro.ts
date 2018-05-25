4073ef0cb35cc1fbd1a6429d283a5605fe749998
JAreina <jareinafdez@gmail.com>
2018-05-25 10:53:42 +0200

    a

---
+++ dos/src/app/entidades/libro.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+
+
+
+export class Libro {
+    
+
+    constructor(public id:string,
+                public titulo:string,
+                public autor:string,
+                public ISBN:string){
+
+    }
+}
