d2b15746ffb3bb99aacb2d8fceb8ae1a5ae4a41a
JAreina <jareinafdez@gmail.com>
2018-05-28 13:55:03 +0200

    a

--- 2-routing/routing/src/app/app.module.ts
+++ 2-routing/routing/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Pagina1Component } from './componentes/pagina1/pagina1.component';
+import { Pagina2Component } from './componentes/pagina2/pagina2.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina1Component,
+    Pagina2Component,
+    CabeceraComponent
   ],
   imports: [
     BrowserModule
