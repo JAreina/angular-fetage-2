a6ab21ea73659f60e50f3d330d5a71547bf38ba4
JAreina <jareinafdez@gmail.com>
2018-05-29 10:40:35 +0200

    router

--- 2-routing/routing/src/app/app.module.ts
+++ 2-routing/routing/src/app/app.module.ts
@@ -5,6 +5,16 @@ import { AppComponent } from './app.component';
 import { Pagina1Component } from './componentes/pagina1/pagina1.component';
 import { Pagina2Component } from './componentes/pagina2/pagina2.component';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const routes: Routes = [
+  { path: 'pagina1', component: Pagina1Component },
+  { path: 'pagina2', component: Pagina2Component },
+  { path: '*', component: AppComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -14,7 +24,8 @@ import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
     CabeceraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
