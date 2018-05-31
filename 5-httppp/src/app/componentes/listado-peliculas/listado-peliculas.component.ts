42eebe8921eff9719e9d237fbb8eee3ac930eaf5
JAreina <jareinafdez@gmail.com>
2018-05-31 10:04:04 +0200

    service

--- 5-httppp/src/app/componentes/listado-peliculas/listado-peliculas.component.ts
+++ 5-httppp/src/app/componentes/listado-peliculas/listado-peliculas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PeliculaService } from '../../servicios/pelicula.service';
 import { Pelicula } from '../../entidades/Pelicula';
 
+
 @Component({
   selector: 'app-listado-peliculas',
   templateUrl: './listado-peliculas.component.html',
@@ -11,21 +12,22 @@ export class ListadoPeliculasComponent implements OnInit {
 
    arryPeliculas : Pelicula[];
    
-  constructor(private peliServ: PeliculaService, private http:Http) { }
+  constructor(private peliServ: PeliculaService) { }
 
   ngOnInit() {
-    this.arryPeliculas = [];
-         this.getPeliculas();
+        this.arryPeliculas=[];
+         this.arryPeliculas = this.peliServ.getPeliculas();
+         console.log("vista listado ");
+       console.log(this.arryPeliculas);
   }
 
   getPeliculas(){
        this.arryPeliculas = this.peliServ.getPeliculas();
+       console.log("vista listado ");
+       console.log(this.arryPeliculas);
   }
 
 
 
-  getPelis(){
-    let observable = this.http.get("url");
-    observable.
-  }
+  
 }
--- 5-httppp/src/app/servicios/pelicula.service.ts
+++ 5-httppp/src/app/servicios/pelicula.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Pelicula } from '../entidades/Pelicula';
 
+import { Pelicula } from '../entidades/Pelicula';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,20 +18,24 @@ export class PeliculaService {
  getPeliculas():any{
    let url:string = "http://localhost:3999/peliculas";
 
-    this.http.get<Pelicula>(url)
-                   .subscribe(data => {   // data is already a JSON object
+     this.http.get<Pelicula[]>(url)
+                   .subscribe(
+                   
+                   data => {   // data is already a JSON object
                        console.log(data);
-                       this.arryPeliculas.push(data);
-                       return this.arryPeliculas;
+                      this.arryPeliculas= data;
+                     return data;
                        
-  });
+                 }, error =>{
+                         console.log(error)
+                  });
    
 
- 
+                  //return this.arryPeliculas;             
  }
 
 
-
+ 
 }
 
 
