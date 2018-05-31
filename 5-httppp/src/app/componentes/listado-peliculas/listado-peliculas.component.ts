c31924a51318d5576c57a775a3ee56ae9db23a2b
JAreina <jareinafdez@gmail.com>
2018-05-31 10:11:36 +0200

    aa

--- 5-httppp/src/app/componentes/listado-peliculas/listado-peliculas.component.ts
+++ 5-httppp/src/app/componentes/listado-peliculas/listado-peliculas.component.ts
@@ -15,16 +15,9 @@ export class ListadoPeliculasComponent implements OnInit {
   constructor(private peliServ: PeliculaService) { }
 
   ngOnInit() {
-<<<<<<< HEAD
-        this.arryPeliculas=[];
-         this.arryPeliculas = this.getPeliculas();
-         console.log("vista listado ");
-       console.log(this.arryPeliculas);
-=======
     this.arryPeliculas = [];
         this.arryPeliculas=  this.peliServ.getPeliculas();
         console.log(this.arryPeliculas)
->>>>>>> 95db6ff3a17d2fba4f684a7c2f2fe75bde0ca0db
   }
 
   getPeliculas(){
@@ -36,12 +29,5 @@ export class ListadoPeliculasComponent implements OnInit {
 
 
 
-<<<<<<< HEAD
   
-=======
-  getPelis(){
-    let observable;
-  
-  }
->>>>>>> 95db6ff3a17d2fba4f684a7c2f2fe75bde0ca0db
 }
